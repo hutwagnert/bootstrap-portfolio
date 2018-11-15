9b69e903e60e9b74da2ab163372b5d1e6fa613ca
Tate Hutwagner <thutwagner@gmail.com>
2018-11-14 19:25:25 -0500

    did some work on css

---
+++ assets/JS/js.js
@@ -0,0 +1,12 @@
+$(document).on("click",'.trivia', function() {
+window.open('https://hutwagnert.github.io/triviagame/trivia-game.html');
+  });
+$(document).on("click",'.giphy', function() {
+window.open('https://hutwagnert.github.io/unit-6/unit-6-giphy/');
+});
+$(document).on("click",'.rpg', function() {
+window.open('https://hutwagnert.github.io/unit-4-game/');
+ });
+$(document).on("click",'.hangman', function() {
+window.open('https://hutwagnert.github.io/word-guess/word-guess.html');
+ });
