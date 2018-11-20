53ead07534618837b88ef1e6599e5ad765cb7023
Tate Hutwagner <thutwagner@gmail.com>
2018-11-20 10:41:47 -0500

    dang

--- assets/JS/js.js
+++ assets/JS/js.js
@@ -10,7 +10,12 @@ window.open('https://hutwagnert.github.io/unit-4-game/');
 $(document).on("click",'.hangman', function() {
 window.open('https://hutwagnert.github.io/word-guess/word-guess.html');
  });
-
+ $(document).on("click",'.rock', function() {
+  window.open('https://hutwagnert.github.io/unit-7/RPS/');
+   });
+   $(document).on("click",'.first', function() {
+    window.open('https://hutwagnert.github.io/TH-G-TT-WW-group-project-1/');
+     });
  $(document).on("click",'.link-button', function() {
   window.open('https://www.linkedin.com/in/tate-hutwagner-b50826131/');
     });
